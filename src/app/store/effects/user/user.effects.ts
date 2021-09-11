3c30af7019c9a5375518d2ea7f2bf6115a9855f0
vinsonf <vfernandez@shift3tech.com>
2021-09-11 09:11:47 -0700

    feat(user): add service

--- src/app/store/effects/user/user.effects.ts
+++ src/app/store/effects/user/user.effects.ts
@@ -1,13 +1,24 @@
 import { Injectable } from '@angular/core';
-import { Actions, createEffect } from '@ngrx/effects';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { EMPTY } from 'rxjs';
+import { catchError, map, mergeMap} from 'rxjs/operators';
+import { UsersService } from 'src/app/services/users.service';
 
 
 
 @Injectable()
 export class UserEffects {
 
+  loadUsers$ = createEffect(() => this.actions$.pipe(
+    ofType('[User] Load Users'),
+    mergeMap(() => this.usersService.getAll()
+      .pipe(
+        map(users => ({ type: '[User] Load Users Success', payload: users })),
+        catchError(() => EMPTY)
+      ))
+    )
+  );
 
-
-  constructor(private actions$: Actions) {}
+  constructor(private actions$: Actions, private usersService: UsersService) {}
 
 }
